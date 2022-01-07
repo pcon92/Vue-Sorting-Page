a6c211c939bfe97c69f0bdd57e56f4a9b841669d
Paul C <paul_congiu@hotmail.com>
2022-01-07 11:47:26 +1100

    minor formatting changes and refactor

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,32 @@
 <template>
-  <bubble-sort
-    v-bind:numbersProp="[4, 2, 6, 8, 2, 9, 1, 2]"></bubble-sort>
+	<bubble-sort :numbersProp="startingNumbers"></bubble-sort>
 </template>
 
 <script>
-import BubbleSort from './components/BubbleSort.vue';
+import BubbleSort from "./components/BubbleSort.vue";
 
 export default {
-  name: 'App',
-  components: {
-    BubbleSort,
-  },
-}
+	name: "App",
+	components: {
+		BubbleSort,
+	},
+	data() {
+		return {
+			startingNumbers: [4, 2, 6, 8, 2, 9, 1, 2],
+		};
+	},
+	methods: {},
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  width: 500px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	width: 500px;
 }
 </style>
--- src/components/BubbleSort.vue
+++ src/components/BubbleSort.vue
@@ -1,71 +1,70 @@
 <template>
-    <h2>Bubble Sort</h2>
-    <ul>
-        <li>Steps through list</li>
-        <li>Compares adjacent elements</li>
-        <li>Swaps them if they are in wrong order</li>
-    </ul>
-    <div id="bubble-sort-numbers">
-        <li v-for="(number, index) in numbers"
-            class="number-div"
-            :key="index">
-            {{ number }}
-        </li>
-    </div>
-    <div>
-        <button @click='startBubbleSort'>Bubble Sort</button>
-    </div>
+	<h2>Bubble Sort</h2>
+	<ul>
+		<li>Steps through list</li>
+		<li>Compares adjacent elements</li>
+		<li>Swaps them if they are in wrong order</li>
+	</ul>
+	<div id="bubble-sort-numbers">
+		<li v-for="(number, index) in numbers" class="number-div" :key="index">
+			{{ number }}
+		</li>
+	</div>
+	<div>
+		<button @click="startBubbleSort">Bubble Sort</button>
+		<button>Replay</button>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'BubbleSort',
-    props: {
-        numbersProp: {
-            type: Array,
-            required: true
-        }
-    },
-    data() {
-        return {
-            numbers: this.numbersProp
-        };
-    },
-    methods: {
-        async startBubbleSort() {
-            let madeSwap;
-            do {
-                madeSwap = false;
-                for (let i=0; i < this.numbers.length; i++) {
-                    if (this.numbers[i + 1] < this.numbers[i]) {
-                        const holdingValue = this.numbers[i+1];
-                        this.numbers[i+1] = this.numbers[i];
-                        this.numbers[i] = holdingValue;
-                        madeSwap = true;
-                        // from top answer https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
-                        await new Promise(r => setTimeout(r, 250));
-                    }
-                }
-            } while (madeSwap);
-        }
-    }
-}
+	name: "BubbleSort",
+	props: {
+		numbersProp: {
+			type: Array,
+			required: true,
+		},
+	},
+	data() {
+		return {
+			numbers: this.numbersProp,
+		};
+	},
+	methods: {
+		async startBubbleSort() {
+			let madeSwap;
+			do {
+				madeSwap = false;
+				for (let i = 0; i < this.numbers.length; i++) {
+					if (this.numbers[i + 1] < this.numbers[i]) {
+						const holdingValue = this.numbers[i + 1];
+						this.numbers[i + 1] = this.numbers[i];
+						this.numbers[i] = holdingValue;
+						madeSwap = true;
+						// from top answer https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
+						await new Promise((r) => setTimeout(r, 250));
+					}
+				}
+			} while (madeSwap);
+		},
+	},
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 #bubble-sort-numbers {
-    display: flex;
-    height: 50px;
-    border: 3px solid black;
+	display: flex;
+	height: 50px;
+	border: 3px solid black;
 }
 .number-div {
-    height: 50px;
-    width: 50px;
-    list-style: none;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border: 1px dotted rgba(0, 0, 0, 0.3);
+	height: 50px;
+	width: 50px;
+	list-style: none;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border: 1px dotted rgba(0, 0, 0, 0.3);
 }
 </style>
